df9662394b2fffede7db44d54637e73536ec25f7
Thiago Soares <thiago.soares@grupoicts.com.br>
2022-09-20 03:25:40 -0400

    second commit

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -2,7 +2,7 @@
     <div id="nav">
         <router-link to="/">Home</router-link>
         <router-link to="/questao/dois">Questao 2</router-link>
-        <router-link to="/questao/cinco">Questao 5</router-link>
+        <router-link to="/questao/seis">Questao 6</router-link>
     </div>
 </template>
 
